7d8899ba8fa239fc7b0c02ca33c5bfd9bd4f112a
CHAN YE <qjdrptmzk@naver.com>
2022-07-19 13:20:08 +0900

    VueSweetalert2

--- vue-shop-pjt_front/src/main.js
+++ vue-shop-pjt_front/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import mixins from './mixins'
-import VueSweetalert2 from 'vue-sweetalert2';
+import VueSweetalert2 from 'vue-sweetalert2';//셋팅까지만
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 createApp(App).mixin(mixins).use(store).use(router).use(VueSweetalert2).mount('#app');
